75ba30788b323f25d4e6cdfc0057de5023a2eef7
prince1998-sudo <puskarbadi860@gmail.com>
2025-04-29 10:00:35 +0530

    Delete SocialImpactConnect directory

--- SocialImpactConnect/client/src/main.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-document.title = "HelpingHands - Social Welfare Organization";
-
-createRoot(document.getElementById("root")!).render(<App />);
